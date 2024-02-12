ca37b6fb64ea7ae9d826a7ef020025533890aaa5
Veeti Uusitalo <AF4694@student.jamk.fi>
2024-02-12 13:17:22 +0200

    Kissa is now done moving on to pilli

--- 01_kissa/_01_Kissa/_01_Kissa/Kissa.cs
+++ 01_kissa/_01_Kissa/_01_Kissa/Kissa.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -12,16 +13,22 @@ namespace _01_Kissa
         private int _age;
         public string _name;
 
+        public override string ToString()
+        {
+            return "Kissa: " + _name + ", ikä " + _age;
+        }
         public Kissa()
         {
             _age = 4;
-            _name = "no name";
+            _name = "Miuku";
         }
         public Kissa(int Age, string Name)
         {
             _age = Age;
             _name = Name;
         }
+        
+
         public void SetCatName(string NewName)
         {
             _name = NewName;
@@ -39,16 +46,48 @@ namespace _01_Kissa
             }
 
         }
-        public void SetCatAge(int NewAge)
+        
+        public bool SetCatAge(int NewAge)
         {
-            if (NewAge >= 0) 
+            if (NewAge >= 0)
             {
                 _age = NewAge;
+                Console.WriteLine("Age has been changed");
+                return true;
             }
-            
+            else
+            {
+                Console.WriteLine("Age was not changed");
+                return false;
+            }
+
+
         }
-        
-        
+
+        /// <summary>
+        /// <c>SetNewCatName</c> method changes Kissa's name but it doesn't accept the name to be hilda.
+        /// </summary>   
+        public bool SetNewCatName(string Newname)
+        {
+            
+            string tempname = Newname;
+            string lowtempname = tempname.ToLower();
+            if (lowtempname == "hilda")
+            {
+                return false;
+            }
+            else
+            {
+                _name = Newname;
+                return true;
+            }
+        }
+
         
     }
 }
+        
+        
+        
+    
+
--- 01_kissa/_01_Kissa/_01_Kissa/Program.cs
+++ 01_kissa/_01_Kissa/_01_Kissa/Program.cs
@@ -5,15 +5,31 @@
         static void Main(string[] args)
         {
             Kissa cat = new Kissa();
-            Kissa kissa = new Kissa(2,"Marko");
+            Kissa kissa = new Kissa(2, "Marko");
             cat._name = "Pekka";
             Console.WriteLine(cat._name);
             cat.SetCatName("Patrick");
             Console.WriteLine(cat._name);
             Console.WriteLine(cat.ReturnCatAge);
-            Console.WriteLine(kissa._name +" "+ kissa.ReturnCatAge);
+            Console.WriteLine(kissa._name + " " + kissa.ReturnCatAge);
             cat.SetCatAge(1);
             Console.WriteLine(cat.ReturnCatAge);
-        }
+            cat.SetNewCatName("Pate");
+            Console.WriteLine(cat._name);
+        
+            {
+                Kissa katti = new Kissa();
+
+                katti.SetNewCatName("Anneli");
+                Console.WriteLine(katti._name);
+                katti.SetNewCatName("Hilda");
+                Console.WriteLine(katti._name);
+            }
+            {
+                Kissa kissa1 = new Kissa();
+                Console.WriteLine(kissa1);
+            }
+        }   
+
     }
 }
