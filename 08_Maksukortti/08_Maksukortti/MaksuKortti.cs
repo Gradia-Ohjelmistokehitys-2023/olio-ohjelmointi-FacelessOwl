4ecd8cff88943d56b686613e7df48014448fef97
Veeti Uusitalo <AF4694@student.jamk.fi>
2024-02-13 13:22:38 +0200

    08_Maksukortti done

---
+++ 08_Maksukortti/08_Maksukortti/MaksuKortti.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Maksukortti
+{
+    public class MaksuKortti
+    {
+        private double saldo;
+        public MaksuKortti(double alkusaldo)
+        {
+            saldo = alkusaldo;
+        }
+        public override string ToString()
+        {
+            return "Kortilla on rahaa " + saldo.ToString("R") +  " euroa";
+        }
+        public void SyoEdullisesti()
+        {
+            if (saldo >= 2.60)
+            {
+                saldo = saldo - 2.60;
+            }
+            
+        }
+        public void SyoMaukkaasti()
+        {
+            if (saldo >= 4.60)
+            {
+                saldo = saldo - 4.60;
+            }
+            
+        }
+        public void LataaRahaa(double lisasaldo)
+        {
+            if (lisasaldo >= 0)
+            {
+                saldo = saldo + lisasaldo;
+            }
+        }
+
+    }
+}
---
+++ 08_Maksukortti/08_Maksukortti/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08_Maksukortti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaksuKortti pekankortti = new MaksuKortti(20);
+            MaksuKortti matinkortti = new MaksuKortti(30);
+            pekankortti.SyoMaukkaasti();
+            matinkortti.SyoEdullisesti();
+            Console.WriteLine("Pekka: "+pekankortti);
+            Console.WriteLine("Matti: "+matinkortti);
+            pekankortti.LataaRahaa(20);
+            matinkortti.SyoMaukkaasti();
+            Console.WriteLine("Pekka: "+pekankortti);
+            Console.WriteLine("Matti: "+matinkortti);
+            pekankortti.SyoEdullisesti();
+            pekankortti.SyoEdullisesti();
+            matinkortti.LataaRahaa(50);
+            Console.WriteLine("Pekka: "+pekankortti);
+            Console.WriteLine("Matti: "+matinkortti);
+        }
+    }
+}
