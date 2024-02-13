a890c2c47e7bcbe53b59fe29f5be69f6bfc44941
Veeti Uusitalo <AF4694@student.jamk.fi>
2024-02-13 09:46:28 +0200

    Velka done

---
+++ 05_Velka/05_Velka/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _05_Velka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Velka pikaVippi = new Velka(1000, 0.12);
+            pikaVippi.TulostaSaldo();
+            pikaVippi.OdotaVuosi();
+            pikaVippi.TulostaSaldo();
+        }
+    }
+}
---
+++ 05_Velka/05_Velka/Velka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Velka
+{
+    public class Velka
+    {
+        public double _saldo;
+        public double _korkoProsentti;
+            public Velka(double saldoAlussa,double korkoProsentti) 
+        {
+            _saldo = saldoAlussa;
+            _korkoProsentti = korkoProsentti;
+        }  
+
+        public void TulostaSaldo()
+        {
+            Console.WriteLine("Saldo: "+_saldo);
+        }
+        public void OdotaVuosi()
+        {
+            _saldo = _saldo * (1 + _korkoProsentti);
+        }
+    }
+}
