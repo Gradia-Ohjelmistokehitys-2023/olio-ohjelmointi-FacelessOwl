b481230c5ffb6f0cde8cce926867394a966e4904
Veeti Uusitalo <AF4694@student.jamk.fi>
2024-02-13 09:28:40 +0200

    Tuote done

---
+++ 04_Tuote/04_Tuote/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _04_Tuote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuote aleTuote = new Tuote("banaani",1.1,13);
+            aleTuote.Tulostatuote();
+        }
+    }
+}
---
+++ 04_Tuote/04_Tuote/Tuote.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Tuote
+{
+    public class Tuote
+    {
+        public double _hinta;
+        public int _lukumaara;
+        public string _name;
+        public Tuote(string Name, double Hinta, int Lukumaara) 
+        { 
+            _name = Name;
+            _hinta = Hinta;
+            _lukumaara = Lukumaara;
+        }
+        
+        public void Tulostatuote()
+        {
+            Console.WriteLine(_name+", hinta "+_hinta+", "+_lukumaara+"kpl");
+        }
+    }
+}
